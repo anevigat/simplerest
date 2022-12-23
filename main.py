8a8a1264a35147bf6b617b12f83d3d6a7d9a91fa
Israel Santana Aleman <isantana@redhat.com>
2022-12-23 13:40:43 +0000

    Simplfy app
    * Re define info as a constant
    * Remove configure route function
    * Refactor tests, use setup.cfg to introduce environment variables

--- main.py
+++ main.py
@@ -6,47 +6,42 @@ import requests
 from flask import Flask
 
 REQUEST_TIMEOUT = 20
+INFO = {
+    'app_name': os.getenv('APP_NAME'),
+    'version': os.getenv('VERSION'),
+    'namespace': os.getenv('NAMESPACE'),
+    'pod': os.getenv('POD'),
+    'node': os.getenv('NODE'),
+    'connect_url': os.getenv('CONNECT_URL'),
+}
 
 app = Flask(__name__)
 
 
-def get_env():
-    """Construct dictionary with Environmental Variables."""
-    info = {}
-    info['app_name'] = os.getenv('APP_NAME')
-    info['version'] = os.getenv('VERSION')
-    info['namespace'] = os.getenv('NAMESPACE')
-    info['pod'] = os.getenv('POD')
-    info['node'] = os.getenv('NODE')
-    info['connect_url'] = os.getenv('CONNECT_URL')
-    return info
+@app.route("/")
+def hello_world():
+    """Define main route."""
+    return f"Hello from {INFO['app_name']}"
 
 
-def configure_routes(app, info):
-    # pylint: disable=redefined-outer-name
-    """Create router."""
-    @app.route("/")
-    def hello_world():
-        return f"Hello from {info['app_name']}"
-
-    @app.route("/info")
-    def get_info():
-        return json.dumps(info)
-
-    @app.route("/connect")
-    def connect():
-        response = {}
-        response['action'] = f"Connecting from {info['app_name']} to {info['connect_url']}"
-        try:
-            request = requests.get(url=info['connect_url'], timeout=REQUEST_TIMEOUT)
-            response['response'] = str(request.content)
-            response['code'] = request.status_code
-        except requests.exceptions.ConnectionError as exc:
-            response['error'] = str(exc.args[0].reason)
-        return json.dumps(response)
+@app.route("/info")
+def get_info():
+    """Info route."""
+    return json.dumps(INFO)
 
 
-configure_routes(app, get_env())
+@app.route("/connect")
+def connect():
+    """Connect with a external service."""
+    response = {}
+    response['action'] = f"Connecting from {INFO['app_name']} to {INFO['connect_url']}"
+    try:
+        request = requests.get(url=INFO['connect_url'], timeout=REQUEST_TIMEOUT)
+        response['response'] = str(request.content)
+        response['code'] = request.status_code
+    except requests.exceptions.ConnectionError as exc:
+        response['error'] = str(exc.args[0].reason)
+    return json.dumps(response)
 
 
 if __name__ == '__main__':
--- tests/test_rest.py
+++ tests/test_rest.py
@@ -1,10 +1,9 @@
 """Test for rest api."""
 import json
+import os
 import unittest
 
-from flask import Flask
-
-from main import configure_routes
+from main import app
 
 
 class TestCalc(unittest.TestCase):
@@ -13,15 +12,7 @@ class TestCalc(unittest.TestCase):
     def setUp(self):
         """Set dictionary for testing and start router."""
         self.info = {}
-        self.info['app_name'] = "Test"
-        self.info['version'] = "v1-test"
-        self.info['namespace'] = "TestNS"
-        self.info['pod'] = "TestPOD"
-        self.info['node'] = "TestNODE"
-        self.info['connect_url'] = "https://www.google.com"
-        self.app = Flask(__name__)
-        configure_routes(self.app, self.info)
-        self.client = self.app.test_client()
+        self.client = app.test_client()
 
     def test_should_return_hello(self):
         """Test for testing base route"""
@@ -34,7 +25,15 @@ class TestCalc(unittest.TestCase):
         """Test for testing /info route"""
         url = '/info'
         response = self.client.get(url)
-        self.assertEqual(str.encode(json.dumps(self.info)), response.get_data())
+        expected = {
+            'app_name': os.environ.get('APP_NAME'),
+            'version': os.environ.get('VERSION'),
+            'namespace': os.environ.get('NAMESPACE'),
+            'pod': os.environ.get('POD'),
+            'node': os.environ.get('NODE'),
+            'connect_url': os.environ.get('CONNECT_URL')
+        }
+        self.assertEqual(str.encode(json.dumps(expected)), response.get_data())
 
     def test_should_return_200_when_external_conn(self):
         """Test for testing /connect route"""
